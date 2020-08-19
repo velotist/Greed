dad808f26a3d7cd8a4428323105aa847fd6c02cd
Marcus Greiner <myeyetea@posteo.de>
2020-08-19 11:04:21 +0200

    Made some smaller changes in output on console

--- Program.cs
+++ Program.cs
@@ -61,7 +61,7 @@ namespace Greed
                 int[] numberOfDice = diceCup.FillDiceCupWithDice(zehntausend.NumberOfDice);
                 foreach (var dice in numberOfDice)
                 {
-                    Console.Write("{0,3}", dice);
+                    Console.Write("{0,-3}", dice);
                 }
 
                 // save occurrences of each eye
@@ -80,35 +80,24 @@ namespace Greed
                     playerIndexInListOfPlayers = zehntausend.GetPlayerIndex(playerIndexInListOfPlayers);
                     player = players[playerIndexInListOfPlayers];
 
-                    Console.WriteLine("Oh no, you have no points at least.");
+                    Console.WriteLine("\tOh no, you have no points at least.");
                     Console.WriteLine();
 
-                    UserInteraction.AwaitKeyAndClearConsole("Press key...");
+                    UserInteraction.AwaitKeyAndClearConsole("\tPress key...");
 
                     continue;
                 }
 
                 totalPoints += points;
 
-                // if same player plays again
-                if (playAgain)
-                {
-                    Console.WriteLine("You now have {0} points, {1}", totalPoints, player.Name);
-                    Console.WriteLine();
-                    Console.WriteLine("You now have {0} dice left to dice with...", zehntausend.NumberOfDice);
-                }
-                else
-                {
-                    Console.WriteLine("You have {0} points, {1}", points, player.Name);
-                    Console.WriteLine();
-                    Console.WriteLine("You have {0} dice left to dice with...", zehntausend.NumberOfDice);
-                }
+                Console.WriteLine(String.Format("{0,10} points", totalPoints));
                 Console.WriteLine();
-
+                Console.WriteLine("{0,10} dice left...", zehntausend.NumberOfDice);
+                Console.WriteLine();
 
                 if (zehntausend.NumberOfDice > 0)
                 {
-                    Console.Write("Continue playing (y/n)? ");
+                    Console.Write("{0,10}, press y if you want to play again... ", player.Name);
                     if (Console.ReadKey().Key == ConsoleKey.Y)
                     {
                         playAgain = true;
@@ -143,7 +132,7 @@ namespace Greed
                 }
             } while (player.Points < 10000);
 
-            Console.Write("Press key...");
+            Console.Write("\tPress key...");
             Console.ReadKey();
             player.Points += totalPoints;
             zehntausend.WinningGame(player);
